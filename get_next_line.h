f1a697e19004b9c6fda1a39950aa1fccdeaa1bfb
agos <agosarias99@gmail.com>
2025-07-12 11:43:05 +0200

    create new branch

--- get_next_line.h
+++ get_next_line.h
@@ -14,22 +14,13 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 100
 # endif
 
-# include <unistd.h>
-# include <stdlib.h>
 # include <stdio.h>
-# include <fcntl.h>
 # include <stddef.h>
-# include <sys/types.h>
+
 
 char	*get_next_line(int fd);
-char	*ft_strdup(char *src);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlen(const char *str);
-char	*ft_strjoin(char const *s1, char const *s2);
-int		ft_index(const char *s, int c);
-void	ft_read(int fd, char **line);
-char	*ft_extract_line(char **line);
+
 #endif
